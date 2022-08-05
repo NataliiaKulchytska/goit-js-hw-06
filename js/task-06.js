0354b2b85ea1bbdae5b9b220b1ecb45b5fa7d14b
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-05 19:56:39 +0300

    task  6

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,22 @@
 const textInput = document.querySelector('#validation-input');
-// console.log(textInput);
+const dataInputAtr = document.querySelector('[data-length]')
+console.log(dataInputAtr);
 
 
 textInput.addEventListener("blur", onCheckContentInput);
 
-function onCheckContentInput() {
-   textInput.textContent.length === textInput[data-length]
-          ? textInput.valid
-          : textInput.invalid;
+function onCheckContentInput(event) {
+   // console.log(event.currentTarget.value.length);
+
+   if (event.currentTarget.value.length === Number(dataInputAtr.dataset.length)) {
+      textInput.classList.add('valid');
+      textInput.classList.remove('invalid')
+   }
+   else {
+       textInput.classList.add('invalid');
+       textInput.classList.remove('valid')
+   }
+   
 };
 
 
